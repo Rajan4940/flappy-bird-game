c7c05c6f95317a19fc4b9e1a3b30db70010c76d5
rajan <rajan49402020@gmail.com>
2023-11-17 10:36:31 +0530

    this is my first 3 web project

--- src/rajanproj_frontend/src/index.js
+++ src/rajanproj_frontend/src/index.js
@@ -1,19 +1,19 @@
-import { rajanproj_backend } from "../../declarations/rajanproj_backend";
+// import { rajanproj_backend } from "../../declarations/rajanproj_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+// document.querySelector("form").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+//   const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
+//   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await rajanproj_backend.greet(name);
+//   // Interact with foo actor, calling the greet method
+//   const greeting = await rajanproj_backend.greet(name);
 
-  button.removeAttribute("disabled");
+//   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+//   document.getElementById("greeting").innerText = greeting;
 
-  return false;
-});
+//   return false;
+// });
